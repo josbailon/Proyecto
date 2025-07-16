09c046f964eb4e0c9f56bcd3b25752f0827e49b5
Franklin Jostin Bailon Palma <jostinbailon53@gmail.com>
2025-07-15 23:16:20 -0500

    Add clinical history components and update view

---
+++ odontologia-admin/src/components/student/ClinicalHistoryForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <HistoryForm
+    :model-value="modelValue"
+    :edit-mode="editMode"
+    @save="$emit('save', $event)"
+    @cancel="$emit('cancel')"
+  />
+</template>
+
+<script setup lang="ts">
+import HistoryForm from './HistoryForm.vue';
+import type { ClinicalHistory } from '../../mocks/student/clinicalHistories';
+
+const props = defineProps<{ modelValue: Partial<ClinicalHistory>; editMode: boolean }>();
+
+defineEmits<{ (e: 'save', data: ClinicalHistory): void; (e: 'cancel'): void }>();
+</script>
+
+<style scoped>
+</style>
